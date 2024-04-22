99ca65d2f909c98fcd2efe1c0cfbdda114d3edd9
fabjopashollari <fabiopashollari15@gmail.com>
2024-04-23 00:03:24 +0200

    I have created the navbar, header, search, login, home page, services page, featured page, discount page, along with their styles ONLY FOR MOBILE for the moment, and their functionalities in JS.

---
+++ E-Books-Web/E-Books-Web/js/index.js
@@ -0,0 +1,120 @@
+/*=============== SEARCH ===============*/
+const searchButton = document.getElementById('search-button');
+const searchClose = document.getElementById('search-close');
+const searchContent = document.getElementById('search-content');
+
+if (searchButton) {
+    searchButton.addEventListener('click', () => {
+        searchContent.classList.add('show-search');
+    });
+}
+
+if (searchClose) {
+    searchClose.addEventListener('click', () => {
+        searchContent.classList.remove('show-search');
+    });
+}
+
+
+/*=============== LOGIN ===============*/
+const loginButton = document.getElementById('login-button');
+const loginClose = document.getElementById('login-close'); 
+const loginContent = document.getElementById('login-content');
+
+if (loginButton) {
+    loginButton.addEventListener('click', () => {
+        loginContent.classList.add('show-login');
+    });
+}
+
+
+if (loginClose) {
+    loginClose.addEventListener('click', () => {
+        loginContent.classList.remove('show-login');
+    });
+}
+
+/*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50  ? header.classList.add('shadow-header')
+                        : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+/*=============== HOME SWIPER ===============*/
+let swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: -24,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        1220:{
+            spaceBetween: -32,
+        },
+    },
+});
+/*=============== FEATURED SWIPER ===============*/
+let swiperFeatured = new Swiper('.featured__swiper', {
+    loop: true,
+    spaceBetween: 16,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints:{
+        1150:{
+            slidesPerView: 4,
+            centeredSlides: false,
+        },
+    },
+});
+
+// swiperFeatured.on('init', function() {
+//     swiperFeatured.on('beforeInit', function(e) {
+//         e.preventDefault(); 
+//     });
+// });
+// Zgjidhni elementët për shigjetat
+// const prevButton = document.querySelector('.swiper-button-prev');
+// const nextButton = document.querySelector('.swiper-button-next');
+
+// // Shtoni event listener për klikun në shigjetën e mbrapa
+// prevButton.addEventListener('click', function(event) {
+//     event.preventDefault(); // Parandaloni sjelljen parazgjedhur të klikimit
+//     swiperFeatured.slidePrev(); // Lëvizni swiper-in mbrapa
+// });
+
+// // Shtoni event listener për klikun në shigjetën përpara
+// nextButton.addEventListener('click', function(event) {
+//     event.preventDefault(); // Parandaloni sjelljen parazgjedhur të klikimit
+//     swiperFeatured.slideNext(); // Lëvizni swiper-in përpara
+// });
+
+/*=============== NEW SWIPER ===============*/
+
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
